a0f9a36bb051a0a4072cc160cd3949b74753ec94
ulumasuip <ulumasuip@yahoo.com>
2023-10-14 18:51:40 -0700

    nave mobile with hamburger and animation

--- lesson1/scripts.js
+++ lesson1/scripts.js
@@ -1,2 +1,15 @@
 const d = new Date();
 document.getElementById("lastModified").innerHTML = d;
+
+const hamburger = document.querySelector(".hamburger");
+const navul = document.querySelector(".navul");
+
+hamburger.addEventListener("click",() => {
+    hamburger.classList.toggle("active");
+    navul.classList.toggle("active");
+})
+
+document.querySelectorAll(".navlink").forEach(n =>n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navul.classList.remove("active");
+}))
