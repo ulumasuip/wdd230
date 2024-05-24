b68e7686534106a324fba6da13009b655b193a9d
ulumasuip <ulumasuip@yahoo.com>
2024-05-24 08:29:04 -0700

    ham menu

--- lesson02/styles/getdates.js
+++ lesson02/styles/getdates.js
@@ -1,2 +1,12 @@
 let text = document.lastModified;
 document.getElementById("date").innerHTML = text;
+
+// Store the selected elements that we are going to use. 
+const mainnav = document.querySelector('.navigation')
+const hambutton = document.querySelector('#menu');
+
+// Add a click event listender to the hamburger button and use a callback function that toggles the list element's list of classes.
+hambutton.addEventListener('click', () => {
+	mainnav.classList.toggle('show');
+	hambutton.classList.toggle('show');
+});
